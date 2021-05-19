1625998828246136b23606a4b2ce33d69ccebe15
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-05-19 15:35:45 -0300

    Finalizada parte II

---
+++ exercises/bloco_4/dia_1/script.js
@@ -0,0 +1,10 @@
+const name = "Guilherme";
+const birthCity = "Belo Horizonte";
+let birthYear = 1988;
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
+// birthCity = "São Paulo";
+console.log(birthCity);
