059afce948e983a79dfdc780031a4a335d976276
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-05-20 17:59:13 -0300

    finalizados exercicios 2 e 3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/2-soma-numeros-array.js
@@ -0,0 +1,11 @@
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (number of numbers) {
+  soma = soma + number;
+}
+
+console.log('A soma é: ' + soma);
