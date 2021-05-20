6a0333be80bddf3cb7e4d9513612ff7992778767
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-05-20 18:42:30 -0300

    finalizados exercicios do bloco 4, dia 2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/8-criando-array.js
@@ -0,0 +1,9 @@
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let newArray = [];
+
+for (index = 1; index <= 25; index += 1) {
+  newArray.push(index);
+}
+
+console.log(newArray);
