821e5f7c299562f70560401c84ceb52bd3af6c5f
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-05-21 17:58:28 -0300

    finalizado exercicio 2 e refatorado exercicio 1

--- exercises/bloco_4/dia_3/exercicio1.js
+++ exercises/bloco_4/dia_3/exercicio1.js
@@ -1,12 +1,12 @@
 // 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 
-let n = 10;
+let n = 5;
 
 // fazer a linha igual a 'n' vezes asterisco
 
-let line = '*';
+let line = '';
 
-for (index = 1; index < n; index += 1) {
+for (index = 0; index < n; index += 1) {
   line = line + '*';
 }
 
---
+++ exercises/bloco_4/dia_3/exercicio2.js
@@ -0,0 +1,15 @@
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+let n = 10;
+
+// fazer a linha igual a 'n' vezes asterisco
+
+let line = '';
+
+for (index = 0; index < n; index += 1) {
+  line = line + '*';
+    console.log(line);
+}
+
+// repetir a linha 'n' vezes
+
