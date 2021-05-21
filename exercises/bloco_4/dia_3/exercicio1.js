00a1958fa87697f56c23931c37a0d7ec395b7361
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-05-21 17:54:38 -0300

    finalizados exemplos e exercicio 1

---
+++ exercises/bloco_4/dia_3/exercicio1.js
@@ -0,0 +1,17 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+let n = 10;
+
+// fazer a linha igual a 'n' vezes asterisco
+
+let line = '*';
+
+for (index = 1; index < n; index += 1) {
+  line = line + '*';
+}
+
+// repetir a linha 'n' vezes
+
+for (indexRepeticao = 0; indexRepeticao < n; indexRepeticao +=1) {
+  console.log(line);
+}
