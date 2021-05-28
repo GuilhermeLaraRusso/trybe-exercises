2901d0e0fdd7e490e6a17f3e4be261f29a92a0e7
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-05-28 18:51:47 -0300

    Executada parte 2 do exemplo

--- exercises/bloco_5/dia3/main.js
+++ exercises/bloco_5/dia3/main.js
@@ -5,15 +5,19 @@ const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
 /*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+1. Copie esse arquivo e edite apenas ele;
+
+2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+
+4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+4.1. Que tal redirecionar para seu portifólio?
+
+5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo;
 
 Segue abaixo um exemplo do uso de event.target:
@@ -30,3 +34,33 @@ divUm.addEventListener('dblclick', resetText);
 // Não precisa passar o parâmetro dentro do addEventListener. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'divUm'.
+
+
+
+
+
+// função para criar a classe tech
+
+function createTech(originEvent) {
+  console.log(originEvent.target);
+
+  // Remove a classe tech existente
+  let techClassToDelete = document.querySelector('.tech');
+  techClassToDelete.classList.remove('tech');
+  
+  // Adiciona uma nova classe tech
+  let techClassToAdd = originEvent.target;
+  techClassToAdd.classList.add('tech');
+}
+
+let clickDiv = document.querySelector('div');
+// let clickDivDois = document.getElementById('divDois');
+
+
+// function recebeClick(eventoDeOrigem) {
+//   console.log(eventoDeOrigem.target);
+//   console.log(eventoDeOrigem.type);
+// }
+
+clickDiv.addEventListener('click', createTech);
+// clickDiv.addEventListener('click', recebeClick)
