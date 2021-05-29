7b006c419ba0b1d45bd4cd54f40ca8a7f413d3a6
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-05-29 09:25:11 -0300

    finalizado exercicio 1. O for esta horrivel, mas esta funcionando

--- exercises/bloco_5/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/bloco_5/javascript-dom-eventos-e-web-storage_3/script.js
@@ -14,3 +14,30 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+let dezDaysItems = document.querySelector('#days');
+
+for (let index = 0; index < dezDaysList.length; index += 1) {
+  let day = dezDaysList[index];
+
+  let dezDaysListDia = document.createElement('li');
+  dezDaysListDia.innerText = day;
+
+  if (day === 24 || day === 25 || day === 31) {
+    dezDaysItems.appendChild(dezDaysListDia).className = 'day holiday';
+  } else {
+    dezDaysItems.appendChild(dezDaysListDia).className = 'day';
+  }
+  
+  if (day === 4 || day === 11 || day === 18 || day === 25) {
+    dezDaysListDia.className = 'day friday';
+  }
+
+  if (day === 25) {
+    dezDaysListDia.className = 'day holiday friday';
+  }
+  
+}
+
