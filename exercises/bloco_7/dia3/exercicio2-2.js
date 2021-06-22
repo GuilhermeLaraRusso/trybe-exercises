d33adbdf36b2acdf0e72656a45549ecdd4e604f1
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-06-22 08:55:03 -0300

    parte 2 dos exercicios

--- exercises/bloco_7/dia3/exercicio2-2.js
+++ exercises/bloco_7/dia3/exercicio2-2.js
@@ -2,9 +2,16 @@ const assert = require('assert');
 // escreva a função wordLengths aqui
 const wordLengths = (array) => {
   let newArr = [];
+  let numberArr = [];
   for (let index = 0; index < array.length; index += 1) {
-    newArr.push(array[index])
+    newArr = array[index].split('');
+    let number = 0;
+    for (let jindex = 0; jindex < newArr.length; jindex += 1) {
+      number += 1;
+    }
+    numberArr.push(number);
   }
+  return numberArr;
 }
 
 const words = ['sun', 'potato', 'roundabout', 'pizza'];
