3724fa3ab3e7adc9ece69b0338fdb636e1a82513
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-06-24 23:25:29 -0300

    exemplos e exercicios. falta exercicio 7

---
+++ exercises/bloco_8/dia2/exemplo1-4-foreach.js
@@ -0,0 +1,13 @@
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
+
+names.forEach((name, index) => {
+  names[index] = name.toLowerCase();
+})
+console.log(names);
