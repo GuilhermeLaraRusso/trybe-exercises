eb63175df088842f37c5a707fb5d604f0efb8960
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-08-17 14:47:33 -0300

    bloco15dia2

---
+++ exercises/bloco_15/dia2/b15d2/src/App.test.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+
+// afterEach(() => jest.clearAllMocks());
+// it('fetch joke', async() => {
+//   const joke = {
+//     id: '7h3oGtrofxc',
+//     joke: 'Whiteboards ... are remarkable',
+//     status: 200,
+//   };
+
+//   jest.spyOn(global, "fetch")
+//   global.fetch.mockResolvedValue({
+//     json: jest.fn().mockResolvedValue(joke),
+//   });
+//   const { findByText } = render (<App />);
+//   await findByText('Whiteboards ... are remarkable');
+//   expect(global.fetch).toBeCalledTimes(1);
+//   expect(global.fetch).toBeCalledWith(
+//     'https://icanhazdadjoke.com/',
+//     {"headers": {"Accept": "application/json"}}
+//   );
+// });
+
+// afterEach(() => jest.clearAllMocks());
+// it('fetch joke', async() => {
+//   const joke = {
+//     id: '7h3oGtrofxc',
+//     joke: 'Whiteboards ... are remarkable',
+//     status: 200,
+//   };
+
+//   global.fetch = jest.fn(() => 
+//   Promise.resolve({
+//     json: () => Promise.resolve(joke)
+//   }));
+
+//   const { findByText } = render(<App />);
+//   await findByText('Whiteboards ... are remarkable');
+//   expect(global.fetch).toBeCalledTimes(1);
+//   expect(global.fetch).toBeCalledWith(
+//     'https://icanhazdadjoke.com/',
+//     {"headers": {"Accept": "application/json"}}
+//   );
+// });
+
+it('alterando o valor dos campos e testando o valor guardado', () => {
+  const { getByTestId } = render(<App />);
+  const inputNome = getByTestId('input-nome');
+  expect(inputNome).toHaveValue('');
+  fireEvent.change(inputNome, { target: { value: 'Estudante da Trybe' } });
+  expect(inputNome).toHaveValue('Estudante da Trybe');
+  
+  // const inputEmail = getByTestId('input-email');
+  // expect(inputEmail).toHaveValue('');
+  // fireEvent.change(inputEmail, { target: { value: 'estudante@trybe.com' } });
+  // expect(inputEmail).toHaveValue('estudante@trybe.com');
+
+  const inputEmail2 = getByTestId('input-email');
+  userEvent.type(inputEmail2, 'guilherme@guilherme');
+  expect(inputEmail2).toHaveValue('guilherme@guilherme')
+})
