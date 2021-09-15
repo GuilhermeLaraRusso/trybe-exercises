9f7beda141e16f99bd9660bb2070778cde693fd4
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-09-15 15:04:30 -0300

    bloco18

---
+++ exercises/bloco_18/dia2/react-hooks/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ComponenteX from './ComponenteX';
+
+function App() {
+  return (
+    <div className="App">
+      <ComponenteX />
+    </div>
+  );
+}
+
+export default App;
