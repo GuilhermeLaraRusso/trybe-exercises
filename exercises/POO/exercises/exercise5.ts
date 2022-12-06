efa33559e4dadf1ac0e257c0e328b74a1a8949b0
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2022-12-06 15:25:30 -0300

    exercicios POO-2

---
+++ exercises/POO/exercises/exercise5.ts
@@ -0,0 +1,24 @@
+// Exercício 5: Escreva uma classe Data cuja instância represente uma data. Esta classe deverá conter três atributos: o dia, o mês e o ano.
+
+// Exercício 6: Vamos adicionar à nossa classe de Data do exercício anterior os comportamentos. Essa classe deverá conter os seguintes métodos:
+
+// constructor: deverá verificar se a data passada por parâmetro é uma data válida, caso não esteja deverá criar uma data com valor “01/01/1900”;
+// getMonthName: retorna o mês da data por extenso;
+// isLeapYear: retorna verdadeiro se o ano é bissexto e falso caso não seja;
+// compare: recebe como parâmetro um outro objeto da classe Data, compara com a data corrente e retorna:
+// 0 se as datas forem iguais;
+// 1 se a data corrente for posterior à data do parâmetro;
+// -1 se a data do parâmetro for posterior à data corrente.
+// format: recebe como parâmetro um formato de dia mês e ano e retorna a data formatada.
+// Legenda:
+// aaaa = ano com quatro dígitos
+// aa = ano com dois dígitos
+// mm = mês com 2 dígitos
+// M = mês por extenso
+// dd = dia com 2 dígitos
+// Exemplos:
+// “dd/mm/aaaa” = 01/01/1900
+// “aaaa-mm-dd” = 1900-01-01
+// “dd de M de aa” = 01 de janeiro de 90
+// “dd, M de aaaa” = 01, janeiro de 1990
+
